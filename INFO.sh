179b7c90eb67da6c29246df050fc5abf3d9fc9a2
ld-2022 <107178363+ld-2022@users.noreply.github.com>
2023-07-13 16:20:48 +0800

    --

--- INFO.sh
+++ INFO.sh
@@ -10,6 +10,7 @@ os_min_ver="7.0-40000"
 maintainer="Synology Inc."
 arch="$(pkg_get_platform)"
 description="this is an example package"
+dsmapp="stars.synology"
 dsmuidir="ui"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	// 静态文件服务器，用于提供前端的HTML、CSS和JavaScript文件。
+	fs := http.FileServer(http.Dir("ui"))
+
+	// 注册路由
+	http.Handle("/", fs)
+
+	// 启动Web服务器
+	http.ListenAndServe(":8080", nil)
+}
